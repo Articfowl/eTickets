8211446b87d4ea47fa5bd63b2b8a62902f679329
Articfowl <40487717+Articfowl@users.noreply.github.com>
2021-10-13 11:35:58 +0200

    Actor functionality

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -11,10 +11,14 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage =" A profile picture is required!")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Full name")]
+        [Required(ErrorMessage = " A full name is required!")]
+        [StringLength(50, MinimumLength =3, ErrorMessage="The full name must be between 3 and 50 characters")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = " A biography is required!")]
         public string Bio { get; set; }
 
         //Relationships
